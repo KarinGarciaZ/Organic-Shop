d124cf412343e4e67a89ef0fb8c5b59d8e9b9d59
KarinGarciaZ <oscarin962011@hotmail.com>
2017-11-02 10:15:45 -0600

    First aproach
    First aproach

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCQsfipIuCjpzqkNaidRR3Dl3Hos6HvgIA",
+    authDomain: "organicshop-2f71e.firebaseapp.com",
+    databaseURL: "https://organicshop-2f71e.firebaseio.com",
+    projectId: "organicshop-2f71e",
+    storageBucket: "",
+    messagingSenderId: "259937259717"
+  }
+};
